4994421181d1e484c805d80a88b8b8e1c5046d76
nickelmagnet <nickelmagnet@gmail.com>
2026-06-21 22:09:14 +0530

    Added a camera and few shapes

--- src/src.cpp
+++ src/src.cpp
@@ -2,13 +2,29 @@
 
 int main()
 {
-    InitWindow(1920, 1080, "BadhiyaWalaApp");
+    InitWindow(1920, 1000, "BadhiyaWalaApp");
+    Camera3D camera = { 0 };
+
+    camera.position = { 2.0f, 2.0f, 2.0f };
+    camera.target = { 0.0f, 1.0f, 0.0f };
+    camera.up = { 0.0f, 1.0f, 0.0f };
+
+    camera.fovy = 60.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
+        UpdateCamera(&camera, CAMERA_FREE);
+        ClearBackground(RAYWHITE);
 
-        ClearBackground(PURPLE);
+        BeginMode3D(camera);
+
+        DrawPlane({ 0,0,0 }, { 20,20 }, GREEN);
+
+        DrawCube({ 0,1,0 }, 2, 2, 2, RED);
+
+        EndMode3D();
 
         EndDrawing();
     }
